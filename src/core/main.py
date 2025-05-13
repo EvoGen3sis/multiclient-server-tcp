bf435a52159073dc34623da8b45ff3773c55b14c
EvoGen3sis <192278290+EvoGen3sis@users.noreply.github.com>
2025-05-13 18:17:38 +0100

    Created server functions.

--- src/core/main.py
+++ src/core/main.py
@@ -1,3 +1,21 @@
 import socket as socket
 
 HOST, PORT = "", 20001
+
+def socket_init():
+    pass
+
+def socket_listen():
+    pass
+
+def socket_handshake():
+    pass
+
+def socket_handle():
+    pass
+
+def echo():
+    pass
+
+def socket_close():
+    pass
