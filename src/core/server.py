e3d276a2752eba419b1fb3e2a6f6baf905c87e7a
EvoGen3sis <192278290+EvoGen3sis@users.noreply.github.com>
2025-05-14 18:11:32 +0100

    Exception handling, improved ux.

--- src/core/server.py
+++ src/core/server.py
@@ -6,8 +6,25 @@ echo_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 echo_sock.bind(addr)
 echo_sock.listen()
 
-while True:
-    cli_sock, cli_addr = echo_sock.accept()
-    data = cli_sock.recv(1024)
-    cli_sock.send(data)
-    cli_sock.close()
+try:
+    while True:
+        cli_sock, cli_addr = echo_sock.accept()
+
+        try:
+            while True:
+                data = cli_sock.recv(1024)
+                
+                if not data:
+                    break
+                else:
+                    cli_sock.send(data)
+        finally:
+            print(f"Client socket closed.")
+            cli_sock.close()
+
+except KeyboardInterrupt:
+    print(f"\tShutting down server...")
+                
+finally:
+    print(f"Server socket closed.")
+    echo_sock.close()
