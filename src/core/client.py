11c4f1b668be5da766f8637f550f6f191672f8b3
EvoGen3sis <192278290+EvoGen3sis@users.noreply.github.com>
2025-05-17 16:07:32 +0100

    Misc.

--- src/core/client.py
+++ src/core/client.py
@@ -1,5 +1,18 @@
 import socket
 
 class Client:
-    def __init__(self):
+    def __init__(self, conn, addr, username, id, message):
+        self.conn = conn
+        self.addr = addr
+        self.username = username
+        self.id = id
+        self.message = message
+
+    def send(self, message):
+     pass
+
+     def recv(self, message):
+        pass
+
+     def start():
         pass
