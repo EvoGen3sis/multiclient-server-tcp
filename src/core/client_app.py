9907f70ca2daab8d89155881e62d5165d5ed5c2b
EvoGen3sis <192278290+EvoGen3sis@users.noreply.github.com>
2025-05-23 01:33:44 +0100

    Commit.

--- src/core/client_app.py
+++ src/core/client_app.py
@@ -3,34 +3,41 @@ import threading
 from datetime import datetime
 from message import Message
 
-host, port = "127.0.0.1", 2119
+class ClientInst:
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.port = 2119
+        self.username = input(f"\nPlease enter your username: ")
 
-def receive(conn):
-    try:
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                break
-            print(f"{data}")
-    finally:
-        conn.close()
+    def receive(self, conn):
+        try:
+            while True:
+                data = conn.recv(1024).decode()
+                now = datetime.now()
+                mssg = Message(self.username, data, now)
+                if not data:
+                    break
+                print(f"{mssg.format}")
+        finally:
+            conn.close()
 
-def connect():
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn.connect((host, port))
-    username = input(f"\nPlease enter your username: ")
-    print(f"username confirmed: @{username}")
-    conn.sendall(username.encode())
-    threading.Thread(target = receive, args = (conn,), daemon = True).start()
-    try:
-        while True:
-            data = input()
-            if data.lower() == "quit":
-               break
-            else:
-               conn.sendall(data.encode())
-    finally:
-        conn.close()
+    def connect(self):
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect((self.host, self.port))
+        username = input(f"\nPlease enter your username: ")
+        print(f"username confirmed: @{username}")
+        conn.sendall(username.encode())
+        threading.Thread(target = self.receive, args = (conn,), daemon = True).start()
+        try:
+            while True:
+                data = input()
+                if data.lower() == "quit":
+                    break
+                else:
+                    conn.sendall(data.encode())
+        finally:
+            conn.close()
 
 if __name__ == "__main__":
-    connect()
+    clientinst = ClientInst()
+    clientinst.connect()
